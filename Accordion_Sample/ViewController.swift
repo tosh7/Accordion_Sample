6c7afecd689db1bfffffb7ee92a9e130e8b233ac
tosh7 <zlia.6.lj.425@gmail.com>
2019-07-20 13:59:49 +0900

    Initial Commit

---
+++ Accordion_Sample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Accordion_Sample
+//
+//  Created by Satoshi Komatsu on 2019/07/20.
+//  Copyright © 2019 Satoshi Komatsu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
